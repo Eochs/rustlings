24a9c5ef691232ac3ebdceb0b7fd4c263e9f2dcc
Spencer Ochs <slochastic@gmail.com>
2019-06-20 11:23:59 -0700

    completed exercises through standard_library_types/arc1.rs'

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -23,7 +23,11 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Ok(f) => Ok(f * cost_per_item + processing_fee),
+        Err(error) => Err(error),
+    }
+    // Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
